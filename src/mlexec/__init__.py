4748144e3ad65b98efbb471f5e4a1790fff0b75d
TRIPA109 <tripa109@umn.edu>
2023-01-18 08:40:28 -0600

    added init scripts for subfolders

--- src/mlexec/__init__.py
+++ src/mlexec/__init__.py
@@ -1,8 +1,6 @@
-from .preprocessing.preprocessing_main import DFImputer
-from .preprocessing.prep_train_data import DFPreprocessor
-from .preprocessing.embedding import DFEmbedder
-from .tuning.modeling import ModelExecutor
-from .evaluation.evaluate import ModelEvaluator
+from .preprocessing import DFImputer, DFPreprocessor, DFEmbedder
+from .tuning import ModelExecutor
+from .evaluation import ModelEvaluator
 from .execution import MLExecutor
 
 from .version import __version__
