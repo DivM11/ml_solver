ef7fb8d6a9fec545c60e5fe2e4d121fd017af763
TRIPA109 <tripa109@umn.edu>
2023-01-20 09:57:31 -0600

    pylance formatting changes

--- src/mlexec/tuning/modeling.py
+++ src/mlexec/tuning/modeling.py
@@ -15,7 +15,7 @@ from hyperopt.pyll.base import scope
 warnings.filterwarnings('ignore')
 
 class ModelExecutor:
-    def __init__(self, 
+    def __init__(self,
                 model_class,
                 x_train: pd.DataFrame,
                 y_train: pd.Series,
@@ -46,14 +46,14 @@ class ModelExecutor:
         self.x_train = x_train
         self.y_train = y_train
         self._task = task
-        
+
         self.primary_metric = metric
         self.class_weights = class_weights
         self._create_scorers()
 
         self.tune_flag = tune_flag
         self.model_type = model_type
-        
+
         if self.model_type == "sklearn":
             if normalize:
                 self.base_model = Pipeline([('norm', StandardScaler()),
@@ -62,7 +62,7 @@ class ModelExecutor:
                 self.base_model = Pipeline([('clf', model_class())])
         else:
             self.model_class = model_class
-        
+
         self.cv = cv
         if self.primary_metric in ["rmse","mae","mape","misclassification_cost"]:
             self.objective = "min"
@@ -70,16 +70,15 @@ class ModelExecutor:
                                 "recall","precision","f1_score","auc_roc","auc_pr"]:
             self.objective = "max"
 
-
         if self.tune_flag:
             self.n_fold = n_fold
             self.adapt_grid(param_grid)
-        
+
         self.internal_val = internal_val
         if internal_val:
             self.x_val = x_val
             self.y_val = y_val
-        
+
         self.fit_params = fit_params
         self.final_train_flag = final_train_flag
 
@@ -91,8 +90,8 @@ class ModelExecutor:
         # Saving the original param space so that we can adapt int and choice
         self.choice_params = {}
         self.int_params = []
-        
-        for k,v in param_grid.items():
+
+        for k, v in param_grid.items():
             modified_key = "clf__"+k
             if type(v) in [str, bool, int, float, dict]:
                 self.param_grid[modified_key] = v
@@ -113,7 +112,7 @@ class ModelExecutor:
                 self.param_grid[modified_key] = hp.randint(modified_key, *v)
             elif isinstance(v[0], float):
                 self.param_grid[modified_key] = hp.uniform(modified_key, *v)
-    
+
     def execute(self, max_evals=40):
         if self.tune_flag:
             if self.cv:
@@ -121,10 +120,10 @@ class ModelExecutor:
                 if self.cv=="nested":
                     outer_cv = KFold(n_splits=5, shuffle=True, random_state=42)
             self.tune_model(max_evals)
-        
+
         if self.final_train_flag:
             self.train_best_model()
-        
+
         if self.cv=="nested":
             # self.tune_flag should be true in this scenario
             self.best_val_results = cross_validate(self.model,
@@ -152,7 +151,7 @@ class ModelExecutor:
             self.best_params[i] = self.choice_params[i][best_index]
         for i in self.int_params:
             self.best_params[i] = round(self.best_params[i])
-    
+
     def get_val_results(self, model):
         val_results = {}
         for metric_name, metric_scorer in self.metric_collection.items():
@@ -166,7 +165,7 @@ class ModelExecutor:
             model = self.base_model.set_params(**params)
         else:
             model = self.model_class(**params)
-        
+
         if self.cv:
             val_results = cross_validate(model,
                                         self.x_train,
@@ -180,16 +179,16 @@ class ModelExecutor:
                     **self.fit_params)
             val_results = self.get_val_results(model)
         loss = val_results["test_"+self.primary_metric].mean()
-        
+
         if self.objective=="max":
             # since this is a minimization opt fn
             loss = loss*-1
-        
+
         return {"loss": loss,
                 'val_results': val_results,
                 'params': params,
                 'status': STATUS_OK}
-    
+
     def train_best_model(self):
         if self.model_type=="sklearn":
             self.model = self.base_model.set_params(**self.best_params)
@@ -199,19 +198,18 @@ class ModelExecutor:
             self.x_train = pd.concat([self.x_train,self.x_val])
             self.y_train = pd.concat([self.y_train,self.y_val])
         self.model.fit(self.x_train, self.y_train, **self.fit_params)
-    
+
     def __repr__(self) -> str:
         if self.model:
             return str(self.model[-1].__class__).split('.')[-1].split("'")[0]
-        else:
-            return str(self.__class__)
-    
+        return str(self.__class__)
+
     @staticmethod
     def convert_metrics_to_scorers(metrics: dict[str, Any]):
-            scorers = {}
-            for k,v in metrics.items():
-                scorers[k] = skmetrics.make_scorer(v)
-            return scorers
+        scorers = {}
+        for k,v in metrics.items():
+            scorers[k] = skmetrics.make_scorer(v)
+        return scorers
 
     @staticmethod
     def get_weighted_predictions(y_test, y_pred, class_weights):
@@ -220,17 +218,20 @@ class ModelExecutor:
         return tn*weights[0], fp*weights[0], fn*weights[1], tp*weights[1]
 
     def misclassification_cost(self, y_test, y_pred):
-        _, w_fp, w_fn, _ = ModelExecutor.get_weighted_predictions(y_test, y_pred, self.class_weights)
+        _, w_fp, w_fn, _ = ModelExecutor.get_weighted_predictions(
+            y_test, y_pred, self.class_weights)
         return (w_fp + w_fn)/len(y_test)
 
     def weighted_accuracy(self, y_test, y_pred):
-        w_tn, w_fp, w_fn, w_tp = ModelExecutor.get_weighted_predictions(y_test, y_pred, self.class_weights)
+        w_tn, w_fp, w_fn, w_tp = ModelExecutor.get_weighted_predictions(
+            y_test, y_pred, self.class_weights)
         return (w_tp+w_tn)/(w_tn+ w_fp+ w_fn+ w_tp)
 
     def weighted_f1_score(self, y_test, y_pred):
-        _, w_fp, w_fn, w_tp = ModelExecutor.get_weighted_predictions(y_test, y_pred, self.class_weights)
+        _, w_fp, w_fn, w_tp = ModelExecutor.get_weighted_predictions(
+            y_test, y_pred, self.class_weights)
         return w_tp / (w_tp+0.5*(w_fp+w_fn))
-    
+
     def _create_scorers(self):
         if self._task=="regression":
             _REGRESSION_METRICS = {
@@ -251,8 +252,9 @@ class ModelExecutor:
             "auc_roc": skmetrics.roc_auc_score,
             "auc_pr": skmetrics.average_precision_score,
             }
-            self.metric_collection = ModelExecutor.convert_metrics_to_scorers(_CLASSIFICATION_METRICS)
-        
+            self.metric_collection = ModelExecutor.convert_metrics_to_scorers(
+                _CLASSIFICATION_METRICS)
+
         # We need to set the primary metric if its none
         if not self.primary_metric:
             self.primary_metric = list(self.metric_collection.keys())[0]
